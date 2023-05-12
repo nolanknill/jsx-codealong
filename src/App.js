9c7d58da324745dc2d02e3873e85d9858429fd0e
Nolan Knill <nolanknill@gmail.com>
2023-05-12 15:31:13 -0400

    Adds Header component, sass package

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <section className="student-list">
+        <div className="student">
+          <div className="student__content">
+            <img
+              className="student__image"
+              src="https://img.icons8.com/ios/100/000000/test-account.png"
+              alt="Test Student Account Image" />
+          </div>
+          <h4 className="student__title">Student Name</h4>
+        </div>
+        <div className="student">
+          <div className="student__content">
+            <img
+              className="student__image"
+              src="https://img.icons8.com/ios/100/000000/test-account.png"
+              alt="Test Student Account Image" />
+          </div>
+          <h4 className="student__title">Student Name</h4>
+        </div>
+        <div className="student">
+          <div className="student__content">
+            <img
+              className="student__image"
+              src="https://img.icons8.com/ios/100/000000/test-account.png"
+              alt="Test Student Account Image" />
+          </div>
+          <h4 className="student__title">Student Name</h4>
+        </div>
+      </section>
+    </>
   );
 }
 
